7e73df04a93185eafd3b7ea659055fc14d4dc635
Sven Liebig <liebigsv@gmail.com>
2017-03-17 00:50:37 +0100

    successful removed the handlers from the main file

---
+++ src/handlers.ts
@@ -0,0 +1,27 @@
+let AboutIntent = function() {
+	let self: Alexa.Handler = this;
+	let speechOutput = 'This skill was writte by Sven Liebig';
+	self.emit(':tellWithCard', speechOutput, 'Svens Skill', speechOutput);
+}
+
+let HelloIntent = function() {
+	let self: Alexa.Handler = this;
+	let intentRequest = <Alexa.IntentRequest> self.event.request;
+	let value = intentRequest.intent.slots.Word.value;
+	let speechOutput = '';
+	if (value.toLowerCase() === 'hello') {
+		speechOutput = 'hello bud';
+	} else {
+		speechOutput = 'say hello scrub';
+	}
+	this.emit(':tellWithCard', speechOutput, 'Svens Skill', speechOutput);
+}
+
+let handlers: Alexa.Handlers = {
+	'AboutIntent': AboutIntent,
+	'HelloIntent': HelloIntent
+};
+
+export function getHandlers() {
+	return handlers;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,11 @@
 import * as Alexa from 'alexa-sdk';
-
-let AboutIntent = function() {
-	let self: Alexa.Handler = this;
-	let speechOutput = 'This skill was writte by Sven Liebig';
-	self.emit(':tellWithCard', speechOutput, 'Svens Skill', speechOutput);
-}
-
-let HelloIntent = function() {
-	let self: Alexa.Handler = this;
-	let intentRequest = <Alexa.IntentRequest> self.event.request;
-	let value = intentRequest.intent.slots.Word.value;
-	let speechOutput = '';
-	if (value.toLowerCase() === 'hello') {
-		speechOutput = 'hello bud';
-	} else {
-		speechOutput = 'say hello scrub';
-	}
-	this.emit(':tellWithCard', speechOutput, 'Svens Skill', speechOutput);
-}
-
-let handlers: Alexa.Handlers = {
-	'AboutIntent': AboutIntent,
-	'HelloIntent': HelloIntent
-};
+import * as handler from './handlers';
 
 export class Handler {
 	constructor(event: Alexa.requestBody, context: Alexa.context, callback: Function) {
 		let alexa = Alexa.handler(event, context);
 		alexa.appId = 'my_alexa_id';
-		alexa.registerHandlers(handlers);
+		alexa.registerHandlers(handler.getHandlers());
 		alexa.execute();
 	}
 }
