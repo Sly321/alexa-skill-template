af036dd394ff54a1d5aaabc2da32f4998fc87578
Sven Liebig <liebigsv@gmail.com>
2017-03-17 00:28:33 +0100

    finally fixed _this problem

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 import * as Alexa from 'alexa-sdk';
 
+let AboutIntent = function() {
+	let self: Alexa.Handler = this;
+	let speechOutput = 'This skill was writte by Sven Liebig';
+	self.emit(':tellWithCard', speechOutput, 'Svens Skill', speechOutput);
+}
+
+let HelloIntent = function() {
+	let self: Alexa.Handler = this;
+	let intentRequest = <Alexa.IntentRequest> self.event.request;
+	let value = intentRequest.intent.slots.Word.value;
+	let speechOutput = '';
+	if (value.toLowerCase() === 'hello') {
+		speechOutput = 'hello bud';
+	} else {
+		speechOutput = 'say hello scrub';
+	}
+	this.emit(':tellWithCard', speechOutput, 'Svens Skill', speechOutput);
+}
+
 let handlers: Alexa.Handlers = {
-	'AboutIntent': () => {
-		let self = Alexa.Handler = this;
-		let speechOutput = 'This skill was writte by Sven Liebig';
-		self.emit('tellWithCard', speechOutput, 'Svens Skill', speechOutput);
-	},
-	'HelloIntent': () => {
-		let self = this;
-		let intentRequest = <Alexa.IntentRequest> self.event.request;
-		let value = intentRequest.intent.slots.Word.value;
-		let speechOutput = '';
-		if (value.toLowercase() === 'hello') {
-			speechOutput = 'hello bud';
-		} else {
-			speechOutput = 'say hello scrub';
-		}
-		self.emit('tellWithCard', speechOutput, 'Svens Skill', speechOutput);
-	},
+	'AboutIntent': AboutIntent,
+	'HelloIntent': HelloIntent
 };
 
 export class Handler {
--- test/index.ts
+++ test/index.ts
@@ -7,33 +7,33 @@ let expect = chai.expect;
 let index = require('../src/index');
 
 let obj = {
-	'session': {
-		'sessionId': 'SessionId.1',
-		'application': {
-			'applicationId': 'my_alexa_id'
-		},
-		'attributes': {},
-		'user': {
-			'userId': null
-		},
-		'new': true
-	},
-	'request': {
-		'type': 'IntentRequest',
-		'requestId': 'EdwRequestId.1',
-		'timestamp': '2017-02-02T22:02:01Z',
-		'intent': {
-			'name': 'HelloIntent',
-			'slots': {
-				'Word': {
-					'name': 'Word',
-					'value': 'hello'
-				}
-			}
-		},
-		'locale': 'de-DE'
-	},
-	'version': '1.0'
+    'session': {
+        'sessionId': 'SessionId.154291c5-a13f-4e7a-ab5a-2342534adfeba',
+        'application': {
+            'applicationId': 'my_alexa_id'
+        },
+        'attributes': {},
+        'user': {
+            'userId': null
+        },
+        'new': true
+    },
+    'request': {
+        'type': 'IntentRequest',
+        'requestId': 'EdwRequestId.474c15c8-14d2-4a77-a4ce-154291c5',
+        'timestamp': '2017-02-02T22:02:01Z',
+        'intent': {
+            'name': 'HelloIntent',
+            'slots': {
+                'Word': {
+                    'name': 'Word',
+                    'value': 'hello'
+                }
+            }
+        },
+        'locale': 'de-DE'
+    },
+    'version': '1.0'
 };
 
 
@@ -41,25 +41,22 @@ describe('HelloIntent', () => {
 
 	let classUnderTest = index;
 
-	let speechResponse = '';
-	let speechError = '';
+    var speechResponse = null;
+    var speechError = null;
 
 	// Vor jedem Test
-	beforeEach((done) => {
+	before((done) => {
 		classUnderTest.Handler(obj, ctx);
 		ctx.Promise
 			.then(response => { speechResponse = response; done(); })
 			.catch(error => { speechError = error; done(); });
 	});
 
-	it('should have something', () => {
-		// Vorbereitung
-
-		// Ausführung
-
-
-		// Prüfung
-		expect(speechError).to.be(null);
-	});
+    describe("asdf", function () {
+        it('should have something', function () {
+            expect(speechResponse.response.card.content).to.be.equal('hello bud');
+            expect(speechError).to.be.equal(null);
+        });
+    });
 
 });
